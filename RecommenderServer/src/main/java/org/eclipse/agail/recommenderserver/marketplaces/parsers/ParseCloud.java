1594bb5cf266bb42118d38bc8bfbc8f84a5a5efe
sedapolaterdeniz <spolater@ist.tugraz.at>
2017-08-01 13:59:43 +0200

    license headers are added

--- RecommenderServer/src/main/java/org/eclipse/agail/recommenderserver/marketplaces/parsers/ParseCloud.java
+++ RecommenderServer/src/main/java/org/eclipse/agail/recommenderserver/marketplaces/parsers/ParseCloud.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (C) 2017 TUGraz.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     TUGraz - initial API and implementation
+ ******************************************************************************/
+
 package org.eclipse.agail.recommenderserver.marketplaces.parsers;
 
 import java.io.IOException;
